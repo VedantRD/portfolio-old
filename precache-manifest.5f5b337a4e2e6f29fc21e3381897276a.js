a51c5049ba9fd8e1a378f2b349a6a5300b1fb1a4
VedantRD <vedant.debadwar@gmail.com>
2020-10-07 15:20:43 +0530

    Updates

---
+++ precache-manifest.5f5b337a4e2e6f29fc21e3381897276a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7680e2a9cf11a508cf2c3403054597",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0ee63ed3e9f2fcaf6db1",
+    "url": "/portfolio/static/css/main.c7c742a0.chunk.css"
+  },
+  {
+    "revision": "977412a116b1a8058615",
+    "url": "/portfolio/static/js/2.c5073eb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.c5073eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee63ed3e9f2fcaf6db1",
+    "url": "/portfolio/static/js/main.5cd19c53.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c91fff83336af5897d91fb4179cc5eff",
+    "url": "/portfolio/static/media/Alice-Regular.c91fff83.ttf"
+  },
+  {
+    "revision": "81b37df3b28d397659607391993eef22",
+    "url": "/portfolio/static/media/Cairo-Regular.81b37df3.ttf"
+  },
+  {
+    "revision": "be1fc370743449c4e2c66eb715d6e261",
+    "url": "/portfolio/static/media/resume2.be1fc370.jpg"
+  }
+]);
